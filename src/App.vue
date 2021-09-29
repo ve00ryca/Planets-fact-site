8015c6eb6f876c50333627562c26d8b82f9839e8
ve00ryca <ema.trayanova@gmail.com>
2021-09-29 11:34:01 +0200

    component NavBar basics

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,101 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavBar />
     <router-view />
   </div>
 </template>
+<script>
+import NavBar from "@/components/NavBar.vue";
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
 
 <style lang="scss">
 /* Css reset */
 @import url("./styles/reset.css");
 /* Google fonts */
-@import url("https://fonts.googleapis.com/css2?family=Antonio&family=Spartan:wght@400;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Antonio:wght@500&family=Spartan:wght@400;700&display=swap");
 
-#app {
+/* Variables */
+$base-color: #e5eff5;
+$background-color: #070724;
+//$border-dark: rgba($base-color, 0.88);
+$mercury-color: #419ebb;
+$venus-color: #eda249;
+$earth-color: #6d2ed5;
+$mars-color: #d14c32;
+$jupiter-color: #d83a34;
+$saturn-color: #cd5120;
+$uranus-color: #1ec1a2;
+$neptune-color: #2d68f0;
+
+/* Base styles */
+html,
+body {
   font-family: "Spartan", sans-serif;
-  font-size: 14px;
+  font-size: 16px;
+  color: $base-color;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 
-#nav {
-  padding: 30px;
+h1,
+h2,
+h3,
+h4,
+button,
+#brand {
+  text-transform: uppercase;
+}
+h1,
+h2,
+#brand {
+  font-family: Antonio, sans-serif;
+  font-weight: 500;
+}
+h1 {
+  font-size: 5rem;
+}
+h2 {
+  font-size: 2.5rem;
+  letter-spacing: -1.5px;
+}
+h3,
+h4,
+button {
+  font-family: Spartan, sans-serif;
+  font-weight: 700;
+}
+h3,
+button {
+  font-size: 0.75rem;
+  letter-spacing: 2.6px;
+}
+button {
+  font-family: Spartan, sans-serif;
+  font-weight: 500;
+  mix-blend-mode: normal;
+  opacity: 0.2;
+  border: 1px solid $base-color;
+  span {
+    font-weight: 700;
+    letter-spacing: 2.57143px;
+    opacity: 0.5;
+  }
+}
+h4 {
+  font-size: 0.6875rem;
+  letter-spacing: 1px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+a {
+  color: $base-color;
+  text-decoration: none;
+  &.router-link-exact-active:not(#brand) {
+    color: #fff;
   }
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,145 @@
+<template>
+  <div>
+    <div class="nav" id="navbar">
+      <router-link to="/" id="brand">THE PLANETS</router-link>
+      <div id="hamburger" @click="isHidden = !isHidden">
+        <span
+          class="hamburger-lines"
+          :class="{ 'hamburger-line-top-transform': !isHidden }"
+          id="hamburger-line-top"
+        ></span
+        ><span
+          class="hamburger-lines"
+          :class="{ 'hamburger-line-middle-transform': !isHidden }"
+          id="hamburger-line-middle"
+        ></span
+        ><span
+          class="hamburger-lines"
+          :class="{ 'hamburger-line-bottom-transform': !isHidden }"
+          id="hamburger-line-bottom"
+        ></span>
+      </div>
+    </div>
+    <ul class="nav" id="nav-items" v-if="!isHidden">
+      <li>
+        <div class="planet-color-circle"></div>
+        <div class="planet-name">Mercury</div>
+        <div class="shevron"></div>
+      </li>
+      <li>
+        <div class="planet-color-circle"></div>
+        <div class="planet-name">Venus</div>
+        <div class="shevron"></div>
+      </li>
+      <li>
+        <div class="planet-color-circle"></div>
+        <div class="planet-name">Earth</div>
+        <div class="shevron"></div>
+      </li>
+      <li>
+        <div class="planet-color-circle"></div>
+        <div class="planet-name">Mars</div>
+        <div class="shevron"></div>
+      </li>
+      <li>
+        <div class="planet-color-circle"></div>
+        <div class="planet-name">Jupiter</div>
+        <div class="shevron"></div>
+      </li>
+      <li>
+        <div class="planet-color-circle"></div>
+        <div class="planet-name">Saturn</div>
+        <div class="shevron"></div>
+      </li>
+      <li>
+        <div class="planet-color-circle"></div>
+        <div class="planet-name">Uranus</div>
+        <div class="shevron"></div>
+      </li>
+      <li>
+        <div class="planet-color-circle"></div>
+        <div class="planet-name">Neptune</div>
+        <div class="shevron"></div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      isHidden: true,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+/* Variables */
+$base-color: #e5eff5;
+
+/* Navigation bar and navigation list for mobile devices */
+.nav {
+  width: 90%;
+  max-width: 327px;
+  margin: 2vh auto;
+  background: black;
+}
+/* Navigation bar with brand and hamburder menu */
+#navbar {
+  position: relative;
+  height: 36px;
+  border-bottom: 1px solid $base-color;
+  #brand {
+    font-size: 1.75rem;
+    letter-spacing: -1.05px;
+    text-align: left;
+  }
+  #hamburger {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 24px;
+    height: 36px;
+  }
+  .hamburger-lines {
+    content: "";
+    position: absolute;
+    right: 0;
+    width: 24px;
+    height: 3px;
+    display: block;
+    background: $base-color;
+    transition: 0.5s ease;
+  }
+  #hamburger-line-top {
+    top: 27.78%;
+  }
+  #hamburger-line-middle {
+    top: 47.22%;
+  }
+  #hamburger-line-bottom {
+    top: 66.67%;
+  }
+  .hamburger-line-top-transform {
+    transform: rotate(-45deg);
+    top: 50% !important;
+  }
+  .hamburger-line-middle-transform {
+    width: 1px;
+    right: 50% !important;
+  }
+  .hamburger-line-bottom-transform {
+    transform: rotate(45deg);
+    top: 50% !important;
+  }
+}
+
+/* Navigation list */
+#nav-items {
+  display: grid;
+  grid-template-columns: 1fr;
+}
+</style>
